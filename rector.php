6c3a06d0f54b934b1f70d86e71e443fd77aa1f66
Pablo Largo Mohedano <devnix.code@gmail.com>
2025-10-09 14:14:46 +0200

    adds makefile and default configurations
    - phpactor
    - php-cs-fixer
    - rector
    - phpstan

---
+++ rector.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withParallel()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true, 
+        typeDeclarationDocblocks: true,
+        privatization: true,
+        naming: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true,
+        rectorPreset: true,
+        phpunitCodeQuality: true,
+        doctrineCodeQuality: true,
+        symfonyCodeQuality: true,
+        symfonyConfigs: true,
+    )
+    ->withPHPStanConfigs([
+        __DIR__ . '/phpstan.dist.neon',
+    ])
+;

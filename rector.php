0cba05b2dc4bdcc49fa6e988fdc19ef0de8937f3
Pablo Largo Mohedano <devnix.code@gmail.com>
2026-02-21 20:19:26 +0100

    remove deprecated `strictBooleans`

--- rector.php
+++ rector.php
@@ -22,7 +22,6 @@ return RectorConfig::configure()
         naming: true,
         instanceOf: true,
         earlyReturn: true,
-        strictBooleans: true,
         rectorPreset: true,
         phpunitCodeQuality: true,
         doctrineCodeQuality: true,
